15572cdc80a664e1ae2c964a5fde2639ed72b26b
Paul Zabelin <paulz@users.noreply.github.com>
2020-04-26 14:58:24 -0700

    create test demonstrating crash

--- CrashInAVAudioRecorderInitTests/CrashInAVAudioRecorderInitTests.swift
+++ CrashInAVAudioRecorderInitTests/CrashInAVAudioRecorderInitTests.swift
@@ -1,34 +1,29 @@
-//
-//  CrashInAVAudioRecorderInitTests.swift
-//  CrashInAVAudioRecorderInitTests
-//
-//  Created by Sparta Science on 4/26/20.
-//  Copyright © 2020 Paul Zabelin. All rights reserved.
-//
-
 import XCTest
-@testable import CrashInAVAudioRecorderInit
+import AVFoundation
 
 class CrashInAVAudioRecorderInitTests: XCTestCase {
-
-    override func setUpWithError() throws {
-        // Put setup code here. This method is called before the invocation of each test method in the class.
+    var validUrl: URL!
+    
+    override func setUp() {
+        super.setUp()
+        validUrl = URL(fileURLWithPath: "/tmp/record.caf")
     }
 
-    override func tearDownWithError() throws {
-        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    func test1SuccessfulInitWithNoSetting() throws {
+        XCTAssertNotNil(try AVAudioRecorder(url: self.validUrl,
+                                            settings: [:]))
     }
 
-    func testExample() throws {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    func test2InitNilWithInvalidFormat() throws {
+        XCTAssertNil(try? AVAudioRecorder(url: self.validUrl,
+                                          settings: [AVFormatIDKey: kAudioFormatMPEGLayer3]))
     }
 
-    func testPerformanceExample() throws {
-        // This is an example of a performance test case.
-        self.measure {
-            // Put the code you want to measure the time of here.
-        }
+    // Causing crash: *** CFEqual() called with NULL first argument ***
+    // in CFEqual.cold.1 ()
+    func test3ExpectingNilActualCrash() throws {
+        let invalidUrlCausingCrash = URL(string: "mailto:bugreport@apple.com")!
+        let recorder = try? AVAudioRecorder(url: invalidUrlCausingCrash, settings: [:])
+        XCTAssertNil(recorder)
     }
-
 }
